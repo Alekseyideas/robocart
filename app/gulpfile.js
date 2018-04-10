fc04d0406f57944ef3388687ac4dbfc02aabb71d
Alekseyideas <alekseyideas@gmail.com>
2018-04-10 13:21:36 +0300

    'init'

---
+++ app/gulpfile.js
@@ -0,0 +1,7 @@
+const   gulp = require('gulp'),
+        pug = require('gulp-pug'),
+        sass = require('gulp-sass'),
+        autoprefixer = require('gulp-autoprefixer'),
+        image = require('gulp-image'),
+        notify = require('gulp-notify'),
+        browserSync = require('browser-sync').create();
