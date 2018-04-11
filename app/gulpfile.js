1126c1d7cdf97d896b66104fa8ff0a3e7d6ba465
Alekseyideas <alekseyideas@gmail.com>
2018-04-11 18:13:37 +0300

    home & offers

--- app/gulpfile.js
+++ app/gulpfile.js
@@ -5,3 +5,65 @@ const   gulp = require('gulp'),
         image = require('gulp-image'),
         notify = require('gulp-notify'),
         browserSync = require('browser-sync').create();
+
+
+const path = {
+  public: './public/',
+  pug: './src/views/',
+  sass: './src/sass/',
+  css: './public/css/',
+  img: './src/img/',
+  img_optimized: './public/img/',
+};
+
+gulp.task('pug', function buildHTML() {
+  return gulp.src(path.pug+'pages/*.pug')
+    .pipe(pug({
+      pretty: true
+    }))
+    .on('error', notify.onError(function (error) {
+      return 'An error occurred while compiling Pug.\nLook in the console for details.\n' + error;
+    }))
+    .pipe(gulp.dest(path.public));
+});
+
+gulp.task('rebuild', ['pug'], function () {
+  browserSync.reload();
+});
+
+gulp.task('browser-sync', ['sass','pug'], function() {
+  browserSync.init({
+    server: path.public
+  });
+});
+
+gulp.task('sass', function () {
+  return gulp.src(path.sass + '**/*.sass')
+    .pipe(sass({
+      includePaths: [path.sass],
+      outputStyle: 'compressed'
+    }))
+    .on('error', sass.logError)
+    .pipe(autoprefixer(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], {
+      cascade: true
+    }))
+    .pipe(gulp.dest(path.css))
+    .pipe(browserSync.reload({
+      stream: true
+    }));
+});
+
+gulp.task('image', function () {
+  gulp.src(path.img+'**/*')
+    .pipe(image())
+    .pipe(gulp.dest(path.img_optimized));
+});
+
+
+
+gulp.task('watch', function () {
+  gulp.watch(path.sass + '**/*.sass', ['sass']);
+  gulp.watch(path.pug + '**/*.pug', ['rebuild']);
+});
+
+gulp.task('default', ['browser-sync', 'watch']);
